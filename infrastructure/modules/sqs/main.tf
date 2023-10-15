b3796f46549a43b0a7cf3dd566a535657fe5d3eb
kenko <kyoshitomi.cursor@gmail.com>
2023-10-15 10:22:54 +0900

    Create the SQS queues

---
+++ infrastructure/modules/sqs/main.tf
@@ -0,0 +1,3 @@
+resource "aws_sqs_queue" "_" {
+  name = local.name
+}
--- infrastructure/service/main.tf
+++ infrastructure/service/main.tf
@@ -5,3 +5,15 @@ module "sns" {
   commons = local.commons
   name    = "new_orders"
 }
+
+module "sqs_inventory" {
+  source  = "../modules/sqs"
+  commons = local.commons
+  name    = "inventory"
+}
+
+module "sqs_analytics" {
+  source  = "../modules/sqs"
+  commons = local.commons
+  name    = "analytics"
+}
